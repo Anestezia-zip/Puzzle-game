0a9047077800a5b2dcb0ba92938f6327e36dab6d
Anestezia-zip <plekatybtc@gmail.com>
2023-08-28 12:37:43 +0100

    Creating pop-up windows

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-
+shufflePictures()
 Puzzle()
 
 
-let countdown;
-$('.start').on('click', function() {
-    allowDragging = true;
-    let countDownDate = new Date().getTime() + 2* 60 * 1000; // Setting the countdown time to 1 minute
-    countdown = setInterval(function startTimer() {
-      let now = new Date().getTime();
-      let timeLeft = countDownDate - now;
-      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000); // number of seconds remaining
-      const formattedTime = (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-      $(".timer-clock").text(formattedTime);
+// -------------------------- Pop-up windows --------------------------
 
-      $('.start').prop("disabled", true).css("opacity", "0.7");
-      $('.check').prop("disabled", false).css("opacity", "1");
-
-      if (timeLeft < 0) { // коли відлік закінчиться
-        clearInterval(countdown);
-        $(".timer-clock").text("Time's up!");
-        $("#start-btn").prop("disabled", false);
-        $("#check-btn").prop("disabled", true);
-      }
-      Puzzle();
+// a question box asking if you really want to check the puzzle
+$('.check').on('click', function(){
+    $('.question-window').css({
+        display: 'block',
+        animation: '0.5s forwards fadeInDown',
     })
+    $('body').css('animation', '1s forwards bgAppear')
 });
 
+// close the question box
+$('.close-btn').on('click', function(){
+    $('.question-window').css({
+        display: 'none',
+        animation: 'none',
+    })
+    $('body').css('animation', 'none')
+});
 
-// Functions
+// ---------------------------- Functions ----------------------------
+
+// Function for dragging and dropping blocks
 let allowDragging = false;
 function Puzzle() {
     $('.number').draggable({
@@ -63,7 +59,40 @@ function Puzzle() {
     });
 }
 
+// Function for the timer and start of the game
+let countdown;
+$('.start').on('click', function() {
+    allowDragging = true;
+    let countDownDate = new Date().getTime() + 2* 60 * 1000; // Setting the countdown time to 1 minute
+    countdown = setInterval(function startTimer() {
+      let now = new Date().getTime();
+      let timeLeft = countDownDate - now;
+      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000); // number of seconds remaining
+      const formattedTime = (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+      $(".timer-clock").text(formattedTime);
 
+      $('.start').prop("disabled", true).css("opacity", ".7");
+      $('.check').prop("disabled", false).css("opacity", "1");
+
+      if (timeLeft < 0) { // коли відлік закінчиться
+        clearInterval(countdown);
+        $(".timer-clock").text("Time's up!");
+        $(".start").prop("disabled", true).css("opacity", ".7");
+        $(".check").prop("disabled", true).css("opacity", ".7");
+      }
+      Puzzle();
+    })
+});
+
+// Function for shuffling picture positions
+function shufflePictures() {
+    let arrRandom = ['0% 0%', '-100% 0%', '-200% 0%', '-300% 0%', '0% -100%', '-100% -100%', '-200% -100%', '-300% -100%', '0% -200%', '-100% -200%', '-200% -200%', '-300% -200%', '0% -300%', '-100% -300%', '-200% -300%', '-300% -300%'];
+        arrRandom = arrRandom.sort(() => Math.random() - 0.5);
+        $('.number').each(function (index, element) {
+            $(this).css('background-position', arrRandom[index]);
+        });
+} 
 
 
 
