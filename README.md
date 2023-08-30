7362c524909cf6a000958c207242a501c7988bac
Anestezia-zip <plekatybtc@gmail.com>
2023-08-30 13:17:39 +0100

    readme file added

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-![Puzzle logo](img/logo.jpg)
+ ## Puzzle Haven <img src="img/logo.jpg" width="20" height="20" />
+
+Welcome to the Puzzle Haven Platform! There are millions of free Haven puzzles created by a large community. You can create, play, share puzzles and compete with other users.  
+
+The live link can be found here - [Puzzle Game Platform](https://anestezia-zip.github.io/Puzzle-game/)
+
+![Responsive Design](img/puzzle-am-i-responsive.jpg)
+
+## Features 
+Puzzle Game is an engaging interactive experience that challenges players with logical puzzles based on drag-and-drop mechanics.
+
+- __Start, Check, New Game Buttons:__  
+The game provides players with the ability to initiate the game using the "Start" button, verify their solution with the "Check" button, and begin a new game session using the "New Game" button.
+![main page](img/readme-screenshot.jpg)
+
+- __Interactive Puzzle Mechanic:__  
+Players can interact with the puzzles by dragging and dropping pieces from the first block to the empty block below, creating an immersive and enjoyable gameplay experience.  
+
+## Future features
+### Header
+In the header, you will find four buttons that are planned to be implemented in the future, enhancing the user experience and providing a more comprehensive puzzle-solving adventure.  
+
+![buttons](img/header-btns.jpg)
+
+- **Puzzles button:**  
+The "Puzzles" button will grant users access to a diverse assortment of puzzle themes. Upon clicking, users will be directed to a page displaying a wide array of puzzles, each categorized by theme, difficulty level, and popularity. This feature will enable users to explore puzzles tailored to their preferences, making their puzzle-solving experience even more engaging and enjoyable.
+- **Levels button:**  
+Clicking the "Levels" button will lead users to a dedicated section where they can choose from various difficulty levels. This functionality aims to provide users with the opportunity to customize the challenge of their puzzles, catering to both beginners and experienced solvers.
+- **Settings button:**  
+ The "Settings" option will empower users to personalize their puzzle environment. Users can adjust sound settings to their liking, enhancing their immersion in the puzzle-solving process. Additionally, a selection of page themes will be available, allowing users to visually tailor their experience to their preferences.
+- **Help button:**  
+The "Help" button will offer valuable resources for users seeking assistance. This section will feature an in-depth FAQ section, addressing common queries and providing prompt solutions. For more individualized support, users will have the option to connect with the support team, allowing for specific inquiries and feedback sharing, contributing to the continual enhancement of the platform.
+### Footer
+There are 3 external links in the footer in the form of Instagram, Twitter, Telegram icons:
+
+- Instagram: Users will be able to follow the latest news, upcoming features and community interaction.
+
+- Twitter: News about new puzzles, upcoming features, and discussions inside Twitter community.
+
+- Telegram: An opportunity to join the Telegram channel to discuss puzzles, share ideas, and keep up to date with the development of the platform.
+
+## Testing 
+### Functionality Testing
+
+I rigorously tested every feature and button to ensure they perform as expected. This included the following:
+
+- Verifying that the Start, Check, and New Game buttons correctly initiate the appropriate actions.
+- Ensuring that the puzzle blocks are randomly generated, not repeated, and positioned appropriately 
+- Confirming that the pop-ups work correctly and work the appropriate logic
+### Compatibility and Responsiveness
+Our website was tested on various devices and browsers to ensure responsiveness and compatibility. We made sure that:
+
+- The website functions seamlessly on different screen sizes, including desktops, tablets, and mobile devices.
+- All buttons and interactive elements are easy to tap and use on touch devices.
+- The website maintains its aesthetics and functionality across popular browsers.
+- I tested that this page works in different browsers: Chrome, Firefox, Safari.
+- Home page are all readable and easy to understand.
+### User Interaction
+It is important to close attention to user interactions to guarantee a user-friendly experience:
+
+- Verified that the drag-and-drop puzzle mechanics work smoothly on both desktop and touch devices.
+- Checked that buttons, links, and navigation are intuitive and easy to understand.
+
+### Validator Testing 
+- HTML
+  - No errors were returned when passing through the official [W3C HTML validator]()
+- CSS
+  - No errors were found when passing through the official [W3C CSS validator](https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fanestezia-zip.github.io%2FPuzzle-game%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en)
+- Javascript
+  - No errors were returned when passing through the official [JSHINT validator](https://jshint.com/)
+![jshint testing](img/jshint-testing.jpg)
+
+- Accessibility
+    - I confirmed that the colors and fonts chosen are easy to read and accessible by running it through lighthouse in devtools  
+![lighthouse](img/puzzle-lighthouse-dekstop.jpg)
+
+## Languages&Tools Used
+
+- HTML5
+- CSS3
+- jQuery
+
+## Deployment 
+
+- The site was deployed to GitHub pages. The steps to deploy are as follows: 
+  - In the GitHub repository, navigate to the Settings tab 
+  - From the source section drop-down menu, select the Main Branch
+  - Once the main branch has been selected, the page will be automatically refreshed with a detailed ribbon display to indicate the successful deployment.
+  
+  The live link can be found here - [Puzzle Game Platform](https://anestezia-zip.github.io/Puzzle-game/)
+
+## Credits 
+
+- The icons in the footer were taken from [Font Awesome](https://fontawesome.com/)
+- A dropdown toggleable menu is from this open Source site [w3schools.com](https://www.w3schools.com/howto/howto_js_dropdown.asp)
--- script.js
+++ script.js
@@ -1,214 +1,203 @@
 $(document).ready(function() {
 
-shufflePictures()
-Puzzle()
-
+    shufflePictures();
+    Puzzle();
 
 // -------------------------- Pop-up windows --------------------------
 
-// a question box asking if you really want to check the puzzle
-$('.check').on('click', function(){
-    $('.question-window').css({
-        display: 'block',
-        animation: '0.5s forwards fadeInDown',
-    })
-    $('body').css('animation', '1s forwards bgAppear')
-});
+    // a question box asking if you really want to check the puzzle
+    $('.check').on('click', function(){
+        $('.question-window').css({
+            display: 'block',
+            animation: '0.5s forwards fadeInDown',
+        });
+        $('body').css('animation', '1s forwards bgAppear');
+    });
 
-// close the question box
-$('.close-btn').on('click', function(){
-    $('.question-window').css({
-        display: 'none',
-        animation: 'none',
-    })
-    $('body').css('animation', 'none')
-});
+    // close the question box
+    $('.close-btn').on('click', function(){
+        $('.question-window').css({
+            display: 'none',
+            animation: 'none',
+        });
+        $('body').css('animation', 'none');
+    });
 
-// Event listener for the 'New game' button
-$(".new-game").on("click", function() {
-    location.reload();
-});
+    // Event listener for the 'New game' button
+    $(".new-game").on("click", function() {
+        location.reload();
+    });
 
-// Сhecking for a well-folded puzzle
-$('.check-btn').on('click', function(){
-    clearInterval(countdown);
-    checkPuzzle();
-    $('.start').prop("disabled", true).css("opacity", "0.7");
-    $('.check').prop("disabled", true).css("opacity", "0.7");
-});
+    // Сhecking for a well-folded puzzle
+    $('.check-btn').on('click', function() {
+        clearInterval(countdown);
+        checkPuzzle();
+        $('.start, .check').prop("disabled", true).css("opacity", "0.7");
+    });
 
 // ---------------------------- Functions ----------------------------
 
-// Function for dragging and dropping blocks
-let allowDragging = false;
-function Puzzle() {
-    $('.number').draggable({
-        containment: 'gamefield',
-        revert: 'invalid',
-        start: function () {
+    // Function for dragging and dropping blocks
+    let allowDragging = false;
+    function Puzzle() {
+        $('.number').draggable({
+            containment: 'gamefield',
+            revert: 'invalid',
+            start: function () {
+                if (!allowDragging) {
+                    return false; // If the flag is set to false, cancel drag and drop
+                }
+                if ($(this).hasClass('dropped-puzzle')) {
+                    $(this).removeClass('dropped-puzzle');
+                    $(this).parent().removeClass('puzzle-present');
+                }
+            }
+        });
+
+        $('.droppable').droppable({
+            accept: function () {
+                return !$(this).hasClass('puzzle-present');
+            },
+            drop: function (event, ui) {
+                let draggableElement = ui.draggable;
+                let droppedOn = $(this);
+                droppedOn.addClass('puzzle-present');
+                $(draggableElement).addClass('dropped-puzzle');
+                $(draggableElement).css({
+                    top: 0,
+                    left: 0,
+                    position: 'relative'
+                }).appendTo(droppedOn);
+            }
+        });
+
+    // Added touch event processing separately
+        $('.number').on('touchstart', function (event) {
             if (!allowDragging) {
-                return false; // If the flag is set to false, cancel drag and drop
+                return false;
             }
             if ($(this).hasClass('dropped-puzzle')) {
                 $(this).removeClass('dropped-puzzle');
                 $(this).parent().removeClass('puzzle-present');
             }
-        }
-    });
-
-    $('.droppable').droppable({
-        accept: function () {
-            return !$(this).hasClass('puzzle-present');
-        },
-        drop: function (event, ui) {
-            let draggableElement = ui.draggable;
-            let droppedOn = $(this);
-            droppedOn.addClass('puzzle-present');
-            $(draggableElement).addClass('dropped-puzzle');
-            $(draggableElement).css({
-                top: 0,
-                left: 0,
+            // Save the coordinates of the touch
+            let touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
+            $(this).data('touch-start-x', touch.pageX);
+            $(this).data('touch-start-y', touch.pageY);
+        }).on('touchmove', function (event) {
+            event.preventDefault();
+            let touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
+            let startX = $(this).data('touch-start-x');
+            let startY = $(this).data('touch-start-y');
+            let offsetX = touch.pageX - startX;
+            let offsetY = touch.pageY - startY;
+            $(this).css({
+                top: offsetY + 'px',
+                left: offsetX + 'px',
                 position: 'relative'
-            }).appendTo(droppedOn);
+            });
+        });
+            
+    }
+
+    // Function for the timer and start of the game
+    let countdown;
+    $('.start').on('click', function() {
+        allowDragging = true;
+        let countDownDate = new Date().getTime() + 2* 60 * 1000; // Setting the countdown time to 1 minute
+        countdown = setInterval(function startTimer() {
+        let now = new Date().getTime();
+        let timeLeft = countDownDate - now;
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000); // number of seconds remaining
+        const formattedTime = (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+        $(".timer-clock").text(formattedTime);
+
+        $('.start').prop("disabled", true).css("opacity", ".7");
+        $('.check').prop("disabled", false).css("opacity", "1");
+
+        if (timeLeft < 0) { // коли відлік закінчиться
+            clearInterval(countdown);
+            $(".timer-clock").text("Time's up!");
+            $(".start").prop("disabled", true).css("opacity", ".7");
+            $(".check").prop("disabled", true).css("opacity", ".7");
         }
+        Puzzle();
+        });
     });
 
-    // Add touch event processing separately
-    $('.number').on('touchstart', function (event) {
-        if (!allowDragging) {
-            return false;
-        }
-        if ($(this).hasClass('dropped-puzzle')) {
-            $(this).removeClass('dropped-puzzle');
-            $(this).parent().removeClass('puzzle-present');
-        }
-        // Save the coordinates of the touch
-        let touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
-        $(this).data('touch-start-x', touch.pageX);
-        $(this).data('touch-start-y', touch.pageY);
-    }).on('touchmove', function (event) {
-        event.preventDefault();
-        let touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
-        let startX = $(this).data('touch-start-x');
-        let startY = $(this).data('touch-start-y');
-        let offsetX = touch.pageX - startX;
-        let offsetY = touch.pageY - startY;
-        $(this).css({
-            top: offsetY + 'px',
-            left: offsetX + 'px',
-            position: 'relative'
-        });
-    })
-        
-}
-
-// Function for the timer and start of the game
-let countdown;
-$('.start').on('click', function() {
-    allowDragging = true;
-    let countDownDate = new Date().getTime() + 2* 60 * 1000; // Setting the countdown time to 1 minute
-    countdown = setInterval(function startTimer() {
-      let now = new Date().getTime();
-      let timeLeft = countDownDate - now;
-      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000); // number of seconds remaining
-      const formattedTime = (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-      $(".timer-clock").text(formattedTime);
-
-      $('.start').prop("disabled", true).css("opacity", ".7");
-      $('.check').prop("disabled", false).css("opacity", "1");
-
-      if (timeLeft < 0) { // коли відлік закінчиться
-        clearInterval(countdown);
-        $(".timer-clock").text("Time's up!");
-        $(".start").prop("disabled", true).css("opacity", ".7");
-        $(".check").prop("disabled", true).css("opacity", ".7");
-      }
-      Puzzle();
-    })
-});
-
-// Function for shuffling picture positions
-function shufflePictures() {
-    let arrRandom = ['0% 0%', '-100% 0%', '-200% 0%', '-300% 0%', '0% -100%', '-100% -100%', '-200% -100%', '-300% -100%', '0% -200%', '-100% -200%', '-200% -200%', '-300% -200%', '0% -300%', '-100% -300%', '-200% -300%', '-300% -300%'];
-        arrRandom = arrRandom.sort(() => Math.random() - 0.5);
-        $('.number').each(function (index, element) {
-            $(this).css('background-position', arrRandom[index]);
-        });
-} 
-
-// Function for checking the puzzle
-let winnerImage = ['0% 0%', '-100% 0%', '-200% 0%', '-300% 0%', '0% -100%', '-100% -100%', '-200% -100%', '-300% -100%', '0% -200%', '-100% -200%', '-200% -200%', '-300% -200%', '0% -300%', '-100% -300%', '-200% -300%', '-300% -300%'];
-function checkPuzzle() {
-    if ($('#droparea .dropped-puzzle').length != 16) {
-        looseWindow();
-        return false;
+    // Function for shuffling picture positions
+    function shufflePictures() {
+        let arrRandom = ['0% 0%', '-100% 0%', '-200% 0%', '-300% 0%', '0% -100%', '-100% -100%', '-200% -100%', '-300% -100%', '0% -200%', '-100% -200%', '-200% -200%', '-300% -200%', '0% -300%', '-100% -300%', '-200% -300%', '-300% -300%'];
+            arrRandom = arrRandom.sort(() => Math.random() - 0.5);
+            $('.number').each(function (index, element) {
+                $(this).css('background-position', arrRandom[index]);
+            });
     } 
-    for (let i = 0; i < 16; i++) {
-        let item = $(`#droparea .dropped-puzzle:eq(${i})`).css('background-position');
-        let order = winnerImage[i];
-        if (item != order) {
+
+    // Function for checking the puzzle
+    let winnerImage = ['0% 0%', '-100% 0%', '-200% 0%', '-300% 0%', '0% -100%', '-100% -100%', '-200% -100%', '-300% -100%', '0% -200%', '-100% -200%', '-200% -200%', '-300% -200%', '0% -300%', '-100% -300%', '-200% -300%', '-300% -300%'];
+    function checkPuzzle() {
+        if ($('#droparea .dropped-puzzle').length != 16) {
             looseWindow();
             return false;
         } 
-        successWindow();
-        return true;
-    };
-};
+        for (let i = 0; i < 16; i++) {
+            let item = $(`#droparea .dropped-puzzle:eq(${i})`).css('background-position');
+            let order = winnerImage[i];
+            if (item != order) {
+                looseWindow();
+                return false;
+            } 
+            successWindow();
+            return true;
+        }
+    }
 
-function looseWindow() {
-    $('.loose-window').css({
-        display: 'block',
-        animation: '0.5s forwards fadeInDown',
-    })
-    $('.question-window').css({
-        display: 'none',
-    })
-    $('.close-btn').on('click', function(){
+    function looseWindow() {
         $('.loose-window').css({
+            display: 'block',
+            animation: '0.5s forwards fadeInDown',
+        });
+        $('.question-window').css({
             display: 'none',
-        })
-    });
-}
+        });
+        $('.close-btn').on('click', function(){
+            $('.loose-window').css({
+                display: 'none',
+            });
+        });
+    }
 
-function successWindow() {
-    $('.success-window').css({
-        display: 'block',
-        animation: '0.5s forwards fadeInDown',
-    })
-    $('.question-window').css({
-        display: 'none',
-    })
-    $('.close-btn').on('click', function(){
+    function successWindow() {
         $('.success-window').css({
+            display: 'block',
+            animation: '0.5s forwards fadeInDown',
+        });
+        $('.question-window').css({
             display: 'none',
-        })
-    });
-}
+        });
+        $('.close-btn').on('click', function(){
+            $('.success-window').css({
+                display: 'none',
+            });
+        });
+    }
 
 // ---------------------------- Dropdown ----------------------------
-
-
-function closeDropdowns() {
-    $(".dropdown-content").removeClass("show");
-}
-
-$(".dropbtn").click(function() {
-    $("#myDropdown").toggleClass("show");
-});
-
-$(document).click(function(event) {
-    if (!$(event.target).closest('.dropdown').length) {
-      closeDropdowns();
+    function closeDropdowns() {
+        $(".dropdown-content").removeClass("show");
     }
-});
 
+    $(".dropbtn").click(function() {
+        $("#myDropdown").toggleClass("show");
+    });
 
-
-
-
-
-
+    $(document).click(function(event) {
+        if (!$(event.target).closest('.dropdown').length) {
+        closeDropdowns();
+        }
+    });
 
 });
 
